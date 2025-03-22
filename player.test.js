bfd6890e7899690cf2d797d67a4c9e2d97ac736e
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-03-22 20:25:45 +0000

    feat(player): Add player class

---
+++ player.test.js
@@ -0,0 +1,16 @@
+import Player from './player';
+import GameBoard from './gameBoard';
+import { expect } from '@jest/globals';
+
+test('player has gameBoard', () => {
+    const player = new Player;
+    expect(player.gameBoard).toBeInstanceOf(GameBoard);
+})
+
+test('player can be human or computer', () => {
+    const human = new Player('human');
+    const computer = new Player('computer');
+
+    expect(human.type).toBe('human');
+    expect(computer.type).toBe('computer');
+})
