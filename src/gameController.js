0364cb1bee6da403416f659ea5d7b9aea5b25ff7
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-04-19 16:38:00 +0100

    feat(gameOver): add isWinner attribute and checkWinner function

--- src/gameController.js
+++ src/gameController.js
@@ -4,7 +4,7 @@ export default class GameController {
     constructor () {
         this.playerOne = new Player('human');
         this.playerTwo = new Player('computer');
-    }
+    };
 
     #randomizeInitialPlayer() {
         const random = Math.floor(Math.random() * 2) + 1;
@@ -13,33 +13,43 @@ export default class GameController {
             this.activePlayer = this.playerOne;
         } else {
             this.activePlayer = this.playerTwo;
-        }
-    }
+        };
+    };
 
     #resetShipsHits() {
         for (let ship of this.playerOne.gameBoard.shipArray) {
             ship.hitsNumber = 0;
             ship.isPlaced = false;
-        }
+        };
 
         for (let ship of this.playerTwo.gameBoard.shipArray) {
             ship.hitsNumber = 0;
             ship.isPlaced = false;
-        }
+        };
+    };
+
+    #checkWinner() {
+        if (this.playerOne.gameBoard.allShipsSunk()) {
+            this.playerTwo.isWinner = true;
+        };
+
+        if (this.playerTwo.gameBoard.allShipsSunk()) {
+            this.playerOne.isWinner = true;
+        };
     }
 
     initialize() {
         this.#randomizeInitialPlayer();
         this.#resetShipsHits();
-    }
+    };
 
     switchPlayer() {
         if (this.activePlayer === this.playerOne) {
             this.activePlayer = this.playerTwo;
         } else {
             this.activePlayer = this.playerOne;
-        }
-    }
+        };
+    };
 
     composeGameBoard() {
         for (let ship of this.playerOne.gameBoard.shipArray) {
@@ -48,8 +58,8 @@ export default class GameController {
                 const col = Math.floor(Math.random() * 10);
                 const orientation = (Math.floor(Math.random() * 2) + 1) === 1 ? 'horizontal' : 'vertical'; 
                 this.playerOne.gameBoard.place(ship, [row, col], orientation)
-            }      
-        }
+            };      
+        };
 
         for (let ship of this.playerTwo.gameBoard.shipArray) {
             while (!ship.isPlaced) {
@@ -57,15 +67,15 @@ export default class GameController {
                 const col = Math.floor(Math.random() * 10);
                 const orientation = (Math.floor(Math.random() * 2) + 1) === 1 ? 'horizontal' : 'vertical'; 
                 this.playerTwo.gameBoard.place(ship, [row, col], orientation);
-            }
-        }
-    }
+            };
+        };
+    };
 
     attackOnPlayerTwo(controller, coordinates, cell) {
         if (controller.activePlayer === controller.playerOne) controller.playerTwo.gameBoard.receiveAttack(coordinates, cell, controller); 
-    }
+    };
 
     attackOnPlayerOne(controller, coordinates, cell) {
         if (controller.activePlayer === controller.playerTwo) controller.playerOne.gameBoard.receiveAttack(coordinates, cell, controller);
-    }
-}
+    };
+};
--- src/player.js
+++ src/player.js
@@ -2,6 +2,7 @@ import GameBoard from "./gameBoard.js"
 
 export default class Player {
     constructor (type) {
+        this.isWinner = false;
         this.gameBoard = new GameBoard();
         this.type = type;
     }
