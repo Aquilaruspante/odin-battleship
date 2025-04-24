1416519ba008b50272730afdf43922edf2bdd43b
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-04-24 21:11:37 +0100

    feat(modalityManager): add manageBoardrendering function

--- src/switchModality.js
+++ src/switchModality.js
@@ -1,9 +1,17 @@
-import { gameController } from "./index.js"
+import { gameController, renderBoardOne, renderBoardTwo } from "./index.js"
 
-export default function modality() {
+function modality() {
     if (gameController.playerTwo.type === 'human') {
         return 'human-human';
     } else if(gameController.playerTwo.type === 'computer') {
         return 'human-computer';
     };
+};
+
+export function manageBoardRendering() {
+    if (modality() === 'human-human' && gameController.activePlayer === gameController.playeOne) {
+        renderBoardOne();
+    } else if(modality() === 'human-human' && gameController.activePlayer === gameController.playerTwo) {
+        renderBoardTwo();
+    };  
 };
